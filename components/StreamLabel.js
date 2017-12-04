fd83f9fd0d2b6cebda71447b0189780c353d00f3
Petr Messner <petr.messner@gmail.com>
2017-12-04 12:19:45 +0100

    sort stream label keys so agent and host are first

--- components/StreamLabel.js
+++ components/StreamLabel.js
@@ -2,12 +2,30 @@ const labelItemStyle = {
   marginRight: 10,
 };
 
-export default ({ label }) => !label ? JSON.stringify(label)+'' : (
-  <span className='StreamLabel' title={JSON.stringify(label)}>
-    {Object.keys(label).map(key => (
-      <span key={key} className='labelItem' style={labelItemStyle}>
-        <b>{key}:</b> {label[key]}
-      </span>
-    ))}
-  </span>
-);
+const compareLabelItems = (a, b) => {
+  if (a.key === 'agent' && b.key !== 'agent') return -1;
+  if (a.key !== 'agent' && b.key === 'agent') return 1;
+  if (a.key === 'host' && b.key !== 'host') return -1;
+  if (a.key !== 'host' && b.key === 'host') return 1;
+  if (a.key < b.key) return -1;
+  if (a.key > b.key) return 1;
+  return 0;
+};
+
+export default ({ label }) => {
+  if (!label) {
+    return (<code>{JSON.stringify(label)+''}</code>);
+  }
+  const items = Object.keys(label).map(key => ({ key, value: label[key] }));
+  // TODO move this sort to fetch function
+  items.sort(compareLabelItems);
+  return (
+    <span className='StreamLabel' title={JSON.stringify(label)}>
+      {items.map(({ key, value }) => (
+        <span key={key} className='labelItem' style={labelItemStyle}>
+          <b>{key}:</b> {value}
+        </span>
+      ))}
+    </span>
+  );
+};
