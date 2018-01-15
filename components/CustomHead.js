78414566c4d1679b0a860f35157aa1d469570712
Petr Messner <petr.messner@gmail.com>
2018-01-15 13:35:49 +0100

    CSS: fix vertical momentum scroll, menu overflow scroll, minor tweaks

--- components/CustomHead.js
+++ components/CustomHead.js
@@ -3,12 +3,12 @@ import Head from 'next/head'
 export default (props) => {
   return (
     <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta http-equiv="cache-control" content="no-cache" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, shrink-to-fit=no" />
       <title>Overwatch monitoring</title>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css" />
-      <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700&amp;subset=latin-ext" rel="stylesheet" />
       <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300,400,500,700&amp;subset=latin-ext" rel="stylesheet" />
-      <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&amp;subset=latin-ext" rel="stylesheet" />
       <link rel="stylesheet" href="/static/main.css" />
     </Head>
   );
